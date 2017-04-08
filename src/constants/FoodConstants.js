280e23f6259596dd2508b484d73cbe7bc1decf92
Kacper Pietrzak <kacpietrzak@gmail.com>
2017-04-08 14:20:51 +0200

    Improving food spawner

--- src/constants/FoodConstants.js
+++ src/constants/FoodConstants.js
@@ -2,3 +2,8 @@ export const MIN_FOOD_VELOCITY = 60;
 export const MAX_FOOD_VELOCITY = 100;
 
 export const FOOD_SPAWN_INTERVAL = Phaser.Timer.SECOND;
+export const FOOD_SPAWN_BOUNDS_WIDTH = 700;
+export const FOOD_SPAWN_BOUNDS_HEIGHT = 500;
+
+export const FOOD_WIDTH = 50;
+export const FOOD_HEIGHT = 50;
--- src/objects/FoodSpawner.js
+++ src/objects/FoodSpawner.js
@@ -1,4 +1,4 @@
-import { FOOD_SPAWN_INTERVAL } from '../constants/FoodConstants';
+import { FOOD_SPAWN_INTERVAL, FOOD_SPAWN_BOUNDS_WIDTH, FOOD_SPAWN_BOUNDS_HEIGHT, FOOD_WIDTH, FOOD_HEIGHT } from '../constants/FoodConstants';
 import Food from './Food';
 
 export default class FoodSpawner extends Phaser.Group {
@@ -8,7 +8,18 @@ export default class FoodSpawner extends Phaser.Group {
     this.game.time.events.loop( FOOD_SPAWN_INTERVAL, this.spawnFood, this );
   }
   spawnFood() {
-    const newFood = new Food( this.game, 10, 10, 'fruit' );
+    const sides = [ 'NORTH', 'EAST', 'SOUTH', 'WEST' ];
+    const spawnSide = sides[ Math.floor( Math.random() * 4 ) ];
+    let x;
+    let y;
+    if ( spawnSide === 'NORTH' || spawnSide === 'SOUTH' ) {
+      x = FOOD_SPAWN_BOUNDS_WIDTH / 2 + Math.random() * FOOD_SPAWN_BOUNDS_WIDTH;
+      y = spawnSide === 'NORTH' ? -FOOD_HEIGHT : this.game.world.height + FOOD_HEIGHT;
+    } else {
+      x = spawnSide === 'WEST' ? -FOOD_WIDTH : this.game.world.width + FOOD_WIDTH;
+      y = FOOD_SPAWN_BOUNDS_HEIGHT / 2 + Math.random() * FOOD_SPAWN_BOUNDS_HEIGHT;
+    }
+    const newFood = new Food( this.game, x, y, 'fruit' );
     this.children.push( newFood );
   }
 }
